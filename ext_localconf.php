95f28a6ff3f90b88e497b1538107f393d3ed6d52
Oliver <bo@cedev.de>
2021-02-22 23:06:35 +0100

    [TASK] Get ready for 11.1

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,7 +9,7 @@ if (!defined('TYPO3_MODE')) {
         \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class)->registerIcon(
             'tx-hotp-icon',
             \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-            ['source' => 'EXT:hotp/Resources/Public/Icons/Extension.svg']
+            ['source' => 'EXT:mfa_hotp/Resources/Public/Icons/Extension.svg']
         );
     }
 );
